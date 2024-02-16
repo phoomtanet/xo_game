dd57de82f6ef50b2c05b0aa60d9676aec99e6e53
phoomtanet <143348885+phoomtanet@users.noreply.github.com>
2024-02-16 12:31:26 +0700

    first commit

--- xo.sql
+++ xo.sql
@@ -13,9 +13,6 @@ CREATE TABLE `game_history` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 
-INSERT INTO `game_history` (`id`, `moves`, `matrixSize`) VALUES
-(1, '[{\"player\":\"X\",\"row\":0,\"col\":1},{\"player\":\"O\",\"row\":1,\"col\":2},{\"player\":\"X\",\"row\":1,\"col\":1}]', 4)
-
 ALTER TABLE `game_history`
   ADD PRIMARY KEY (`id`);
 
